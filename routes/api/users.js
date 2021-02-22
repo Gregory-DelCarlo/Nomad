b5b01c89ebed6f26b52a23bf1ee8ece021683fc6
Bogey <jeffreysbogart@gmail.com>
2021-02-22 14:17:04 -0800

    Add login post router to users routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
@@ -30,6 +31,27 @@ router.post('/register', (req, res) => {
           })
         })
       }
+    })
+})
+
+router.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ username: 'This user does not exist' });
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({ msg: 'Success' });
+          } else {
+            return res.status(400).json({ password: 'Incorrect password' });
+          }
+        })
     })
 })
 
