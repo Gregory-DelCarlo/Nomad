772ee7cf6cc98023ffed42993be02e20b21c0018
Bogey <jeffreysbogart@gmail.com>
2021-02-22 11:29:07 -0800

    Setup express routing

--- nomad.js
+++ nomad.js
@@ -2,18 +2,25 @@ const express = require('express');
 const nomad = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const bodyParser = require('body-parser');
+
+nomad.use('/api/users', users);
 
 // set up root route
 nomad.get('/', (req, res) => res.send('entry working'));
 
 //set local and production port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 //connect database to mongoose for use
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+nomad.use(bodyParser.urlencoded({ extended: false }));
+nomad.use(bodyParser.json());
 
 //set server so listen on the port specified
 nomad.listen(port, () => console.log(`Server is running on port ${port}`));
