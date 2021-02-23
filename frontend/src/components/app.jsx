45f27d58b4ce070cc1f347f01552736f13129b22
Trevor Smith <trevor.smith1667@gmail.com>
2021-02-23 11:29:33 -0800

    working on  branch

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import NavBarContainer from './nav/navbar_container';
-
 import MainPage from './main_page/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
--- frontend/src/components/main_page/main_page.jsx
+++ frontend/src/components/main_page/main_page.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Map from '../search/map';
 
 export default class MainPage extends React.Component {
     render() {
         return (
             <div>
-                <h1>Main Page</h1>
+                <Map/>
             </div>
         )
     }
