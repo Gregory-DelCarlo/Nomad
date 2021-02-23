c9fbddd11e7fa4bac525d746ba1260e51a3c0a27
Bogey <jeffreysbogart@gmail.com>
2021-02-22 21:47:39 -0800

    Add login and signup AuthRoutes to app.jsx

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import NavBarContainer from './nav/navbar_container';
+
 import MainPage from './main_page/main_page';
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
     <Switch>
         <Route exact path='/' component={MainPage}/>
+        <AuthRoute exact path="/login" component={LoginFormContainer} />
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
     </Switch>
 );
 
