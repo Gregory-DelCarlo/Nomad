834020cd9ab773ee2e58317e249b02e0255e2378
Bogey <jeffreysbogart@gmail.com>
2021-02-22 21:27:09 -0800

    Add session_reducer and import it in root reducer

--- frontend/src/reducers/root_reducer.jsx
+++ frontend/src/reducers/root_reducer.jsx
@@ -1,5 +1,8 @@
-const RootReducer = () => {
+import { combineReducers } from 'redux';
+import sessionReducer from './session_reducer';
 
-}
+const RootReducer = combineReducers({
+  session: sessionReducer
+});
 
 export default RootReducer;
