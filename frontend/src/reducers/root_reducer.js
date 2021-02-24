092baf03dc0e75d1a299d2f934ef7166515555ee
appkenny <kzeng.95@gmail.com>
2021-02-24 01:08:03 -0800

    created modal action and reducer as well as an ui reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import sessionReducer from './session_reducer';
 import ErrorsReducer from './errors_reducer';
+import uiReducer from './ui_reducer';
 
 const RootReducer = combineReducers({
   session: sessionReducer,
-  errors: ErrorsReducer
+  errors: ErrorsReducer,
+  ui: uiReducer,
 });
 
 export default RootReducer; 
