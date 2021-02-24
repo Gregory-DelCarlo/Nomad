2a49a9a8bbfdf9fbe3eaa9a4bbd6df0e8bb6de77
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-02-24 09:28:23 -0800

    Attempting to fix heroku

--- nomad.js
+++ nomad.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'production') {
 
   
 const passport = require('passport');
-nomad.get('/', (req, res) => res.send('entry working'));
+// nomad.get('/', (req, res) => res.send('entry working'));
 
 //set local and production port
 const port = process.env.PORT || 5000;
@@ -38,7 +38,7 @@ nomad.use('/api/users', users);
 nomad.use('/api/parks', parks);
 
 // set up root route
-nomad.get('/', (req, res) => res.send('entry working'));
+// nomad.get('/', (req, res) => res.send('entry working'));
 
 //set server so listen on the port specified
 nomad.listen(port, () => console.log(`Server is running on port ${port}`));
