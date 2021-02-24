0a525c4551ce04d35bb23dd9677e47e801527c50
Bogey <jeffreysbogart@gmail.com>
2021-02-24 14:26:06 -0800

    remove debuggers

--- frontend/src/components/backpack/backpack_container.jsx
+++ frontend/src/components/backpack/backpack_container.jsx
@@ -1,4 +1,8 @@
 import { connect } from 'react-redux';
 import Backpack from './backpack';
 
+// const mapStateToProps = state => ({
+//   loggedIn: state.session.isAuthenticated
+// });
+
 export default connect(null, null)(Backpack);
--- frontend/src/util/route_util.js
+++ frontend/src/util/route_util.js
@@ -2,28 +2,30 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route, Redirect } from 'react-router-dom';
 
+const mapStateToProps = state => {
+    return { loggedIn: Boolean(state.session.id) };
+};
+
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
     <Route
         path={path}
         exact={exact}
         render={props =>
-            loggedIn ?  <Redirect to="/" /> : <Component {...props} />
+            loggedIn ? <Redirect to="/backpack" /> : <Component {...props} /> // change backpack to home once the home component is built
         }
     />
 );
-const Protected = ({ component: Component, path, loggedIn, exact }) => (
-    <Route
-        path={path}
-        exact={exact}
-        render={props =>
-            loggedIn ? <Component {...props} /> : <Redirect to="/" /> 
-        }
-    />
-);
-
-const mapStateToProps = state => {
-    return { loggedIn: Boolean(state.session.id) };
-};
+const Protected = ({ component: Component, path, loggedIn, exact }) => {
+    return (
+        <Route
+            path={path}
+            exact={exact}
+            render={props =>
+                loggedIn ? <Component {...props} /> : <Redirect to="/" /> 
+            }
+        />
+    )
+}
 
 export const AuthRoute = withRouter(connect(mapStateToProps,null)(Auth));
 export const ProtectedRoute = withRouter(connect(mapStateToProps,null)(Protected));
