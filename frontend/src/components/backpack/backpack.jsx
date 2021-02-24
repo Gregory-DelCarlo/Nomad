c32d787d294b13d9266233cc905c0a5725a56c6b
Bogey <jeffreysbogart@gmail.com>
2021-02-23 23:19:37 -0800

    Add currentPage and numItems to backpack state, and write code for getItems function

--- frontend/src/components/backpack/backpack.jsx
+++ frontend/src/components/backpack/backpack.jsx
@@ -3,20 +3,38 @@ import React from 'react';
 class Backpack extends React.Component {
   constructor(props) {
     super(props);
-    
+    this.state = {
+      currentPage: 'main',
+      numItems: 0
+    }
     this.getItems = this.getItems.bind(this);
   }
 
   getItems() {
-    return (
-      <div>
-
-      </div>
-    )
+    const itemNum = this.state.numItems;
+    if (itemNum > 0) {
+      const itemList = ["Time & Location", "Team", "Supplies"];
+      const items = itemList.slice(0, itemNum);
+      return (
+        <ul className="item-list">
+          {items.map(item => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      )
+    }
   }
 
+
+
   render() {
-    return null;
+    return (
+      <div className="backpack-page">
+        <div className="backpack">
+          {this.getItems()}
+        </div>
+      </div>
+    )
   }
 }
 
