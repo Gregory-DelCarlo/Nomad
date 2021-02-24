8020a17ae2d7f50d9d5d34108d467320be4208f2
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-02-23 23:25:09 -0800

    Removed map rendering from main page

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -7,13 +7,16 @@ import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
-    <Switch>
-        <Route exact path='/' component={MainPage}/>
-        <AuthRoute exact path="/login" component={LoginFormContainer} />
-        <AuthRoute exact path="/signup" component={SignupFormContainer} />
-        {/* create home (or other) component and add protect routes here */}
-        {/* add navbar route, possibly protected */}
-    </Switch>
+    <div>
+        <NavBarContainer />
+        <Switch>
+            <Route exact path='/' component={MainPage}/>
+            <AuthRoute exact path="/login" component={LoginFormContainer} />
+            <AuthRoute exact path="/signup" component={SignupFormContainer} />
+            {/* create home (or other) component and add protect routes here */}
+            {/* add navbar route, possibly protected */}
+        </Switch>
+    </div>
 );
 
 export default App;
