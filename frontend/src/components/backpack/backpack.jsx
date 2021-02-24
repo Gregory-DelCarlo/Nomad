7aa5692dcaad873176d7a1502159ccc8b02f87a7
Bogey <jeffreysbogart@gmail.com>
2021-02-23 22:42:45 -0800

    Add skeleton for getItems function to backpack

---
+++ frontend/src/components/backpack/backpack.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Backpack extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.getItems = this.getItems.bind(this);
+  }
+
+  getItems() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default Backpack;
---
+++ frontend/src/components/backpack/backpack_container.jsx
@@ -0,0 +1,4 @@
+import { connect } from 'react-redux';
+import Backpack from './backpack';
+
+export default connect(null, null)(Backpack);
