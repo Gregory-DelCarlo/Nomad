634288dd4c95a7b2d82d324147383abc5aabdbdf
appkenny <kzeng.95@gmail.com>
2021-02-25 11:14:24 -0800

    added a carousel to the main page and some css

--- frontend/src/components/main_page/main_page.jsx
+++ frontend/src/components/main_page/main_page.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
-
-
+import Carousel from 'react-bootstrap/Carousel'
+import 'bootstrap/dist/css/bootstrap.min.css'
 export default class MainPage extends React.Component {
     render() {
         return (
-            <main>
-                <h1>
-                    Welcome to our Site!!!
-                </h1>
-                <div>Really cool splash page here!</div>
-                {/* <ParksDropDownContainer /> */}
-            </main>
+            <div className="carousel-container">
+                <div className="carousel-header">
+                    Plan Your Trip Today
+                </div>
+                <Carousel>
+                <Carousel.Item interval={2500}>
+                  <img
+                    className="carosel-item"
+                    src="https://media.tenor.com/images/b143ac6c86210714b4273f28c05f3aa7/tenor.gif"
+                  />
+                  <Carousel.Caption>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={2500}>
+                  <img
+                    className="carosel-item"
+                    src="https://media.tenor.com/images/b143ac6c86210714b4273f28c05f3aa7/tenor.gif"
+                  />
+                  <Carousel.Caption>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item  interval={2500}>
+                  <img
+                    className="carosel-item"
+                    src="https://media.tenor.com/images/b143ac6c86210714b4273f28c05f3aa7/tenor.gif"
+                  />
+                  <Carousel.Caption>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                </Carousel>
+            </div>
         )
     }
 }
