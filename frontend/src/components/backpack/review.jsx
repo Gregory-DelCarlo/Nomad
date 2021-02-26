4d413dc860a3f54803839b5cc924686809caf19e
Bogey <jeffreysbogart@gmail.com>
2021-02-26 12:57:53 -0800

    Add styling to review page

--- frontend/src/components/backpack/review.jsx
+++ frontend/src/components/backpack/review.jsx
@@ -3,28 +3,33 @@ import React from 'react';
 const Review = ({ reviewBackpack }) => (
   <div className='review'>
     <div className='review-box'>
-      <h1>Review</h1>
-      <h2>{reviewBackpack.title}</h2>
+      <h1>Review {reviewBackpack.title}</h1>
       <div className='review-team'>
-        <h3>Team</h3>
-        <div className="team-list">
+        <h2>Team:</h2>
+        <ul className="team-list">
           {reviewBackpack.team.map((member, index) => (
             <li key={index}>{member}</li>
           ))}
-        </div>
+        </ul>
       </div>
       <div className='review-supplies'>
-        <h3>Supplies</h3>
+        <h2>Supplies:</h2>
         <div className='review-supplies-box'>
           <div className='food-list'>
-            {reviewBackpack.food.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
+            <label>Food</label>
+            <ul>
+              {reviewBackpack.food.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
           </div>
           <div className='equipment-list'>
-            {reviewBackpack.equipment.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
+            <label>Equipment</label>
+            <ul>
+              {reviewBackpack.equipment.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
