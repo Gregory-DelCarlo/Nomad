6a1e8002b8fa781e7eaa4fd17ed8e91824e078d5
Trevor Smith <trevor.smith1667@gmail.com>
2021-03-23 16:54:42 -0700

    refactored calendar

--- frontend/src/components/calendar/calendar.js
+++ frontend/src/components/calendar/calendar.js
@@ -3,13 +3,12 @@ import FullCalendar, { formatDate } from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-
-
-
+import {Sidebar} from './calendar_sidebar'
 
 export default class Calendar extends React.Component {
     constructor(props) {
         super(props)
+        this.handleWeekendsToggle = this.handleWeekendsToggle.bind(this)
     }
     
     state = {
@@ -23,7 +22,11 @@ export default class Calendar extends React.Component {
     render() {
         return (
             <div className='calendar-whole'>
-                {this.renderSidebar()}
+                    <Sidebar
+                        toggled={this.state.weekendsVisible}
+                        onChange={this.handleWeekendsToggle}
+                        trips={this.props.trips}
+                    />
                 <div className='calendar-main'>
                     <FullCalendar
                         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
@@ -37,14 +40,12 @@ export default class Calendar extends React.Component {
                         selectable={true}
                         selectMirror={true}
                         dayMaxEvents={true}
-                         weekends={this.state.weekendsVisible}
+                        weekends={this.state.weekendsVisible}
                         initialEvents={this.props.trips}
                         eventClick={this.handleEventClick}
                         eventsSet={this.handleEvents} 
-                        
-                        
-                        // called after events are initialized/added/changed/removed
-                        /* you can update a remote database when these fire:
+                    // called after events are initialized/added/changed/removed
+                    /* you can update a remote database when these fire:
                     eventContent={renderEventContent} // custom render function
                     eventAdd={function(){}}
                     eventChange={function(){}}
@@ -56,30 +57,6 @@ export default class Calendar extends React.Component {
         )
     }
 
-    renderSidebar() {
-        return (
-            <div className='calendar-sidebar'>
-                <div className='calendar-sidebar-section'>
-                    <label>
-                        <input
-                            type='checkbox'
-                            // checked={this.props.weekendsVisible}
-                            checked={this.state.weekendsVisible}
-
-                            onChange={this.handleWeekendsToggle}
-                        ></input>
-                            toggle weekends
-                    </label>
-                </div>
-                <div className='calendar-sidebar-section'>
-                    <h2>All Hikes({this.props.trips.length})</h2>
-                    <ul>
-                        {this.props.trips.map(renderSidebarEvent)}
-                    </ul>
-                </div>
-            </div>
-        )
-    }
     
     handleWeekendsToggle = () => {
         this.setState({
@@ -89,15 +66,7 @@ export default class Calendar extends React.Component {
    
 }
 
-function renderSidebarEvent(event) {
-    return (
-        <li key={event.id}>
-            <b>{formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}</b>
-            <i>{event.title}</i>
-        </li>
-    )
-}
-// function renderEventContent(eventInfo) {
+// // function renderEventContent(eventInfo) {
 //     return (
 //         <>
 //             <b>{eventInfo.timeText}</b>
